e75511da1f33fa3711c0f763faa2b1d081f12eef
Ivan da Silva Rosa <iivan.si@msn.com>
2022-01-12 00:11:14 -0300

    add diretorios backend e frontend

---
+++ backend/src/main/java/com/devsuperior/dsrmovie/DsrmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsrmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsrmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsrmovieApplication.class, args);
+	}
+
+}
