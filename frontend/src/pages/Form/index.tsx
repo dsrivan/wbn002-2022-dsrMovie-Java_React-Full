79be87777428ca7951ed53448ab950f2ef134034
Ivan da Silva Rosa <iivan.si@msn.com>
2022-01-12 23:32:36 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 function Form() {
@@ -40,7 +41,11 @@ function Form() {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary dsrmovie-btn mt-3">Cancelar</button>
+        <Link to="/">
+          <button className="btn btn-primary dsrmovie-btn mt-3">
+            Cancelar
+          </button>
+        </Link>
       </div>
     </div>
   );
